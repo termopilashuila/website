34211c3a1fbcb42ba062399036eb03779318e091
Camilo Cabrera <cecabrera55@gmail.com>
2025-03-13 19:48:19 -0500

    Clean up old assets and improve build process

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 module.exports = {
   entry: './src/ts/main.ts',
@@ -19,4 +20,16 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap('CopyServiceWorker', (compilation) => {
+          const srcPath = path.resolve(__dirname, 'src/js/service-worker.js');
+          const destPath = path.resolve(__dirname, 'dist/service-worker.js');
+          fs.copyFileSync(srcPath, destPath);
+          console.log('Service worker copied to dist directory');
+        });
+      }
+    }
+  ]
 }; 
