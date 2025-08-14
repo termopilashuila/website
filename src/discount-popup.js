a669d4bd7d022f3f87ca78f0deec8e1ec6bfac23
Camilo Cabrera <cecabrera55@gmail.com>
2025-08-14 12:00:26 -0500

    Update discount popup configuration to reduce timer duration from 40 seconds to 30 seconds for improved user experience.

--- src/discount-popup.js
+++ src/discount-popup.js
@@ -10,7 +10,7 @@ class DiscountPopupHandler {
         // Configuration with defaults
         this.config = {
             popupDelay: options.popupDelay || 10000, // 10 seconds
-            timerDuration: options.timerDuration || 40, // 40 seconds
+            timerDuration: options.timerDuration || 30, // 30 seconds
             backendUrl: options.backendUrl || 'https://script.google.com/macros/s/AKfycbwXCkqjA3d4Lj26TWXCHHgg16Z6UYdahdh4fP5kWVqUs0wCokys_TASoiy2Sr5GwktN3g/exec',
             popupId: options.popupId || 'discount-popup',
             ...options
